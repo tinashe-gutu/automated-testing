fd8def6b580a9e3c41d2fa3a165ec75d731e2b10
Tinashe Gutu <tinashegutu2011@gmail.com>
2022-10-25 13:02:23 +0100

    add 2 test suites for finding earliest string and returning earlier string

---
+++ src/find-shortest-string.test.js
@@ -0,0 +1,11 @@
+const findLongestString = require("./find-shortest-string");
+
+test("findShortesttString finds the shortest string in an array", () => {
+  expect(findLongestString(["it", "is", "a", "nice", "day"])).toBe("a");
+  expect(findLongestString(["why", "hello", "to", "you"])).toBe("to");
+});
+
+test("findShortestString returns the earlier string in cases of joint shortest strings", () => {
+  expect(findLongestString(["brave", "dance"])).toBe("brave");
+  expect(findLongestString(["hello", "jest", "hallo"])).toBe("hello")
+});
