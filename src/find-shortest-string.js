99fc8de9a14a9eb49412ee542e4c656a97903328
Tinashe Gutu <tinashegutu2011@gmail.com>
2022-10-25 12:39:52 +0100

    remove manual tests from find-shortest-string.js

--- src/find-shortest-string.js
+++ src/find-shortest-string.js
@@ -17,6 +17,5 @@ function findShortestString(stringArr) {
   return shortestString.length;
 }
 
-console.log(findShortestString(["it", "is", "a", "nice", "day"]), "a");
-console.log(findShortestString(["why", "hello", "to", "you"]), "to");
-console.log(findShortestString(["brave", "dance"]), "brave");
+
+
